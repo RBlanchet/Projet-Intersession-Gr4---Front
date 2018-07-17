4caaa37ee30b35fd94ae4f28590f560ae8cbb266
Ivan Armagnat <ivan.armagnat@gmail.com>
2018-07-17 11:29:32 +0200

    Router and routing example

--- source/components/login.js
+++ source/components/login.js
@@ -1,19 +1,67 @@
-import * as React from "react"
+import { withFormik } from 'formik';
+import Yup from 'yup';
 
-export default class Ratings extends React.Component {
-    constructor(props) {
-        super(props)
-    }
+// Our inner form component. Will be wrapped with Formik({..})
+const Login = props => {
+    const {
+        values,
+        touched,
+        errors,
+        dirty,
+        isSubmitting,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        handleReset,
+    } = props;
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="email" style={{ display: 'block' }}>
+                Email
+            </label>
+            <input
+                id="email"
+                placeholder="Enter your email"
+                type="text"
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className={errors.email && touched.email ? 'text-input error' : 'text-input'}
+            />
+            {errors.email &&
+            touched.email && <div className="input-feedback">{errors.email}</div>}
 
-    render() {
-        return (
-            <article className="test">
-                <p className="">
-                    <input type="text"/>
-                    <input type="text"/>
-                </p>
-            </article>
-        )
+            <button
+                type="button"
+                className="outline"
+                onClick={handleReset}
+                disabled={!dirty || isSubmitting}
+            >
+                Reset
+            </button>
+            <button type="submit" disabled={isSubmitting}>
+                Submit
+            </button>
 
-    }
-}
+            <DisplayFormikState {...props} />
+        </form>
+    );
+};
+
+const EnhancedForm = withFormik({
+    mapPropsToValues: () => ({ email: '' }),
+    validationSchema: Yup.object().shape({
+        email: Yup.string()
+            .email('Invalid email address')
+            .required('Email is required!'),
+    }),
+    handleSubmit: (values, { setSubmitting }) => {
+        setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+        }, 1000);
+    },
+    displayName: 'BasicForm', // helps with React DevTools
+})(Login);
+
+export default EnhancedForm
--- source/main_renderer.js
+++ source/main_renderer.js
@@ -1,8 +1,36 @@
-import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import React from "react"
+import {HashRouter as Router, Route, Link, Redirect} from "react-router-dom"
 
-import Login from './components/login'
+import Nav from "./components/nav"
+
+const Main = () => (
+    <Router>
+        <div>
+            <Nav/>
+
+            <hr/>
+
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+        </div>
+    </Router>
+)
+
+const Home = () => (
+    <div>
+        <h2>Home</h2>
+    </div>
+)
+
+
+const About = () => (
+    <div>
+        <h2>About</h2>
+    </div>
+)
+
 
 const main = document.getElementById("main")
 
-ReactDOM.render(<Login/>, main)
+ReactDOM.render(<Main/>, main)
