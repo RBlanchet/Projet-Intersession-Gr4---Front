398cb7a933bc6797c5641a1d0fe64ee62c109a1d
Maxime Delaporte <mdelaporte@clever-age.com>
2018-07-30 02:02:21 +0200

    Modifying tasks form

--- source/components/tasksForm.js
+++ source/components/tasksForm.js
@@ -31,6 +31,47 @@ const Form = (props) => {
                         props.touched.name &&
                         <div className="input-feedback">{props.errors.name}</div>}
                     </div>
+
+                    <div className={"form__input-block"}>
+                        <label htmlFor="name" className={"form__label"}>
+                            Date de début
+                        </label>
+                        <input
+                            id="start_at"
+                            placeholder="2000-01-01"
+                            type="date"
+                            value={props.values.start_at}
+                            onChange={props.handleChange}
+                            onBlur={props.handleBlur}
+                            className={props.errors.start_at && props.touched.start_at
+                                ? 'text-input error'
+                                : 'text-input'}
+                        />
+                        {props.errors.start_at &&
+                        props.touched.start_at &&
+                        <div className="form__error">{props.errors.start_at}</div>}
+                    </div>
+
+                    <div className={"form__input-block"}>
+                        <label htmlFor="name" className={"form__label"}>
+                            Date de fin
+                        </label>
+                        <input
+                            id="end_at"
+                            placeholder="2000-01-01"
+                            type="date"
+                            value={props.values.end_at}
+                            onChange={props.handleChange}
+                            onBlur={props.handleBlur}
+                            className={props.errors.end_at && props.touched.end_at
+                                ? 'text-input error'
+                                : 'text-input'}
+                        />
+                        {props.errors.end_at &&
+                        props.touched.end_at &&
+                        <div className="form__error">{props.errors.end_at}</div>}
+                    </div>
+
                     <div className={"form__input-block"}>
                         <label htmlFor="name" style={{display: 'block'}}>
                             Description de la tache
@@ -50,92 +91,14 @@ const Form = (props) => {
                         <div className="input-feedback">{props.errors.description}</div>}
                     </div>
 
-                    <div className={"form__input-block"}>
-                        <label htmlFor="name" className={"form__label"}>
-                            Date de début
-                        </label>
-                        <input
-                            id="date_start"
-                            placeholder="2000-01-01"
-                            type="date"
-                            value={props.values.date_start}
-                            onChange={props.handleChange}
-                            onBlur={props.handleBlur}
-                            className={props.errors.date_start && props.touched.date_start
-                                ? 'text-input error'
-                                : 'text-input'}
-                        />
-                        {props.errors.date_start &&
-                        props.touched.date_start &&
-                        <div className="form__error">{props.errors.date_start}</div>}
-                    </div>
 
-                    <div className={"form__input-block"}>
-                        <label htmlFor="name" className={"form__label"}>
-                            Heure de début
-                        </label>
-                        <input
-                            id="hour_start"
-                            placeholder="00:00"
-                            type="text"
-                            value={props.values.hour_start}
-                            onChange={props.handleChange}
-                            onBlur={props.handleBlur}
-                            className={props.errors.hour_start && props.touched.hour_start
-                                ? 'text-input error'
-                                : 'text-input'}
-                        />
-                        {props.errors.hour_start &&
-                        props.touched.hour_start &&
-                        <div className="form__error">{props.errors.hour_start}</div>}
-                    </div>
-
-                    <div className={"form__input-block"}>
-                        <label htmlFor="name" className={"form__label"}>
-                            Date de fin
-                        </label>
-                        <input
-                            id="date_end"
-                            placeholder="2000-01-01"
-                            type="date"
-                            value={props.values.date_end}
-                            onChange={props.handleChange}
-                            onBlur={props.handleBlur}
-                            className={props.errors.date_end && props.touched.date_end
-                                ? 'text-input error'
-                                : 'text-input'}
-                        />
-                        {props.errors.date_end &&
-                        props.touched.date_end &&
-                        <div className="form__error">{props.errors.date_end}</div>}
-                    </div>
-
-                    <div className={"form__input-block"}>
-                        <label htmlFor="name" className={"form__label"}>
-                            Heure de fin
-                        </label>
-                        <input
-                            id="hour_end"
-                            placeholder="00:00"
-                            type="text"
-                            value={props.values.hour_end}
-                            onChange={props.handleChange}
-                            onBlur={props.handleBlur}
-                            className={props.errors.hour_end && props.touched.hour_end
-                                ? 'text-input error'
-                                : 'text-input'}
-                        />
-                        {props.errors.hour_end &&
-                        props.touched.hour_end &&
-                        <div className="form__error">{props.errors.hour_end}</div>}
-                    </div>
                     <div className={"form__input-block"}>
                         <label htmlFor="name" style={{display: 'block'}}>
                             Statut de la tache
                         </label>
                         <select
                             id="status"
-                            value="1"
+                            value={props.values.status}
                             onChange={props.handleChange}
                             onBlur={props.handleBlur}
                             className={props.errors.status && props.touched.status
@@ -143,10 +106,10 @@ const Form = (props) => {
                                 : 'text-input'
                             }
                         >
-                            <option value="1">A faire</option>
-                            <option value="2">En cours</option>
-                            <option value="3">Terminée</option>
-                            <option value="4">Validée</option>
+                            <option value='to_do'>A faire</option>
+                            <option value='in_progress'>En cours</option>
+                            <option value='finished'>Terminée</option>
+                            <option value='validated'>Validée</option>
                         </select>
                         {props.errors.status &&
                         props.touched.status &&
@@ -176,19 +139,19 @@ const Form = (props) => {
                             Heure alloué à la tache
                         </label>
                         <input
-                            id="hours_spend"
-                            placeholder="Temps passé"
+                            id="time_spend"
+                            placeholder="Temps passé (optionnel)"
                             type="number"
-                            value={props.values.hours_spend}
+                            value={props.values.time_spend}
                             onChange={props.handleChange}
                             onBlur={props.handleBlur}
-                            className={props.errors.hours_spend && props.touched.hours_spend
+                            className={props.errors.time_spend && props.touched.time_spend
                                 ? 'text-input error'
                                 : 'text-input'}
                         />
-                        {props.errors.hours_spend &&
-                        props.touched.hours_spend &&
-                        <div className="form__error">{props.errors.hours_spend}</div>}
+                        {props.errors.time_spend &&
+                        props.touched.time_spend &&
+                        <div className="form__error">{props.errors.time_spend}</div>}
                     </div>
                     <div className={"form__buttons"}>
                         <button className={"form__button"} type="submit" disabled={props.isSubmitting}>
@@ -201,7 +164,7 @@ const Form = (props) => {
                             </button>
                             : <button className={"form__button form__button--red"} onClick={props.setEditing(false)}
                                       type="button">
-                                Anuller
+                                Annuler
                             </button>}
                     </div>
                 </form>
@@ -210,89 +173,80 @@ const Form = (props) => {
                 </button>
             </div>
         </div>
-                )
+    )
 
-                }
+}
 
-                class TasksForm extends React.Component {
+class TasksForm extends React.Component {
 
-                handleSubmit(values, {setSubmitting}) {
-                setTimeout(() => {
-                const changed = diff(this.initialValues, values)
-                console.log(apiHelpers.apiPatch("tasks/" + this.editing, values))
-                // alert(JSON.stringify(changed, null, 2))
-                setSubmitting(false)
-            }, 100)
-            }
-                    deleteTask(id) {
-                        return () => {
-                            apiHelpers.apiDelete("tasks", id).then(response => {
-                                console.log(response)
-                                this.setEditing(false)()
-                                // TODO: feedback
-                            })
-                        }
-                    }
-                render() {
+    handleSubmit(values, {setSubmitting}) {
+        setTimeout(() => {
+            const changed = diff(this.initialValues, values)
+            console.log(apiHelpers.apiPatch("tasks/" + this.editing, values))
+            // alert(JSON.stringify(changed, null, 2))
+            setSubmitting(false)
+        }, 100)
+    }
 
-                let editingTask
-                if (this.props.editing === "new") {
-                editingTask = false
-            } else {
-                editingTask = this.props.tasks.entities.tasks[this.props.editing]
-            }
-                return (
-                <div>
+    deleteTask(id) {
+        return () => {
+            apiHelpers.apiDelete("tasks", id).then(response => {
+                console.log(response)
+                this.setEditing(false)()
+                // TODO: feedback
+            })
+        }
+    }
+
+    render() {
+
+        let editingTask
+        if (this.props.editing === "new") {
+            editingTask = false
+        } else {
+            editingTask = this.props.tasks.entities.tasks[this.props.editing]
+        }
+        return (
+            <div>
                 <Formik
-                {...this.props}
-                validationSchema={
-                    object().shape({
-                        name: string()
-                            .required('Vous devez saisir un nom pour votre tache !'),
-                        description: string()
-                            .required('Vous devez saisir une description pour votre tâche !'),
-                        status: string()
-                            .required('Vous devez préciser un statut pour votre tâche !'),
-                        hour_start: string()
-                            .matches(/^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/, "Votre date n'est pas au bon format"),
-                        date_start: string()
-                            .required('Vous devez saisir une date'),
-                        hour_end: string()
-                            .matches(/^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/, "Votre date n'est pas au bon format"),
-                        date_end: string()
-                            .required('Vous devez saisir une date'),
-                        cost: string()
-                            .required('Le prix ne peut être négatif'),
-                        hour_pool: string()
-                            .matches(/^\d+$/, "Ce n'est pas un format d'heure valide"),
-                        hours_spend: string()
-                            .required('Le nombre d\'heures ne peut etre négatif'),
-
-                    })
-                }
-                onSubmit={this.handleSubmit}
-                initialValues={{
-                    name: editingTask.name,
-                    date_start: editingTask.date_start ? editingTask.date_start.substr(0, 10) : null,
-                    hour_start: editingTask.date_start ? editingTask.date_start.substr(11, 5) : null,
-                    date_end: editingTask.date_end ? editingTask.date_end.substr(0, 10) : null,
-                    hour_end: editingTask.date_end ? editingTask.date_end.substr(11, 5) : null,
-                    cost: editingTask.cost,
-                    hour_spend: editingTask.hour_spend,
-                    description: editingTask.description
-                }}
-                render={formikProps =>
-                    <Form {...formikProps} displayName={"UsersInnerForm"}
-                          setEditing={this.props.setEditing}
-                          editing={this.props.editing}
-                          deleteTask={this.deleteTask}/>
-                }
+                    {...this.props}
+                    validationSchema={
+                        object().shape({
+                            name: string()
+                                .required('Vous devez saisir un nom pour votre tache !'),
+                            description: string()
+                                .required('Vous devez saisir une description pour votre tâche !'),
+                            status: string()
+                                .required('Vous devez préciser un statut pour votre tâche !'),
+                            start_at: string()
+                                .required('Vous devez saisir une date'),
+                            end_at: string()
+                                .required('Vous devez saisir une date'),
+                            cost: string()
+                                .required('Le prix ne peut être négatif'),
+                        })
+                    }
+                    onSubmit={this.handleSubmit}
+                    initialValues={{
+                        name: editingTask.name,
+                        start_at: editingTask.start_at ? editingTask.start_at.substr(0, 10) : null,
+                        end_at: editingTask.end_at ? editingTask.end_at.substr(0, 10) : null,
+                        cost: editingTask.cost,
+                        time_spend: editingTask.hour_spend,
+                        description: editingTask.description,
+                        status: editingTask.status,
+                    }}
+                    render={formikProps =>
+                        <Form {...formikProps} displayName={"TasksInnerForm"}
+                              setEditing={this.props.setEditing}
+                              editing={this.props.editing}
+                              deleteTask={this.deleteTask}/>
+                    }
                 />
 
-                </div>
-                )
+            </div>
+        )
+    }
+}
 
-            }
-            }
-
-                export default TasksForm
+export default TasksForm
